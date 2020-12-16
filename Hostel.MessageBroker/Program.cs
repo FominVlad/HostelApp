859d0152f09281405757759b3d51175a495fd031
FominVlad <mr.vladyslavfomin@gmail.com>
2020-12-16 23:21:11 +0200

    Add broker and listener

--- Hostel.MessageBroker/Program.cs
+++ Hostel.MessageBroker/Program.cs
@@ -1,28 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Producer;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Autofac.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Net;
 
 namespace Hostel.MessageBroker
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            //FanoutExchangePublisher.Publish(channel);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                //.UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:7777/");
+            listener.Start();
+            Console.WriteLine("Ожидание подключений...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();/*await listener.GetContextAsync();*/
+                HttpListenerRequest request = context.Request;
+                var a = context.Request.Headers.GetValues("json").GetValue(0);
+                HttpListenerResponse response = context.Response;
+
+                string responseString = "<html><head><meta charset='utf8'></head><body>Привет мир!</body></html>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+        }
     }
 }
--- Hostel.MessageListener/Program.cs
+++ Hostel.MessageListener/Program.cs
@@ -1,28 +1,20 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Consumer;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Autofac.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Hostel.MessageListener
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            FanoutExchangeConsumer.Consume(channel);
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                //.UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
